29f904f7208063bc11b5595381f0d97db9373d63
ventsy95 <ventsy95@mail.bg>
2016-08-29 12:38:01 +0300

    Methods implementation change to match the return types from IAdmin. Static methods deleteAd and addUser return types changed to boolean. Static method listAllUsers's return type changed to List<String>.

--- source/Administrator.java
+++ source/Administrator.java
@@ -1,7 +1,8 @@
 package source;
 
-
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -9,29 +10,25 @@ import exceptions.NoSuchUserException;
 
 public class Administrator extends User {
 	private String adminUserName;
-    private static Map<String, User> allUsers;
-	//ArrayList<Ad> allAds = new ArrayList<Ad>(); nqma nujda ot vsichki obqvi	
-	
+	private static Map<String, User> allUsers;
 
 	private Administrator(String name, String phoneNumber, String address, String email) {
 		super(name, phoneNumber, address, email);
 		this.adminUserName = "admin";
-		Administrator.allUsers = new HashMap<String, User >();
+		Administrator.allUsers = new HashMap<String, User>();
 	}
 
 	private static Administrator instance = new Administrator("Admin", "0878372222", "Sofia", "admin@gmail.com");
 
- 
-	// Get the only object available
 	public static Administrator getInstance() {
 		return instance;
 	}
 
-	
-	public static void banUser(User user) {
+	public static boolean banUser(User user) {
 		for (String key : allUsers.keySet()) {
 			if (key.equals(user.getUserName())) {
 				allUsers.remove(key);
+				return true;
 			} else {
 				try {
 					throw new NoSuchUserException("This user doesn't exist in our database.");
@@ -41,29 +38,38 @@ public class Administrator extends User {
 			}
 
 		}
+		return false;
 	}
-	
-	
-	public static void deleteAd(Ad ad, User user) {
-		for (String key : allUsers.keySet()) {
-			if (key.equals(user.getUserName())) {
-				user.deleteAd(ad);
+
+	public static boolean deleteAd(Ad ad, User user) {
+		if (ad != null && user != null) {
+			for (String key : allUsers.keySet()) {
+				if (key.equals(user.getUserName())) {
+					user.deleteAd(ad);
+				}
 			}
-		}
-	}
-	
-	public static void addUser(User user) {
-		if(user != null ) {
-			allUsers.put(user.getUserName(), user);
-		}
-		
-	}
-	
-	public static void listAllUsers(){
-		for (Entry<String, User> user : allUsers.entrySet()) {
-			System.out.println("User: "+ user.getKey());
+			return true;
+		} else {
+			return false;
 		}
 	}
 
+	public static boolean addUser(User user) {
+		if (user != null) {
+			allUsers.put(user.getUserName(), user);
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+
+	public static List<String> listAllUsers() {
+		List<String> listAllUsers= new ArrayList<String>();
+		for (Entry<String, User> user : allUsers.entrySet()) {
+			listAllUsers.add(user.getKey());
+		}
+		return listAllUsers;
+	}
 
 }
